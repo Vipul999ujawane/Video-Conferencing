cec0253e770bb86542b14d6222f5080c23d7b6f8
Vipul999ujawane <vipul999ujawane@gmail.com>
2017-10-05 19:05:35 +0530

    Fix

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 var express = require('express');
 var app = express();
-var ExpressPeerServer = require('peer').ExpressPeerServer;
 
 //app.get('/', function(req, res, next) { res.sendFile('static/index.html'); });
 
-var server = app.listen(9000);
+var server = app.listen(8000);
 
-var options = {
-    debug: true
-}
-
-app.use('/peerjs', ExpressPeerServer(server, options));
 app.use(express.static('static'));
